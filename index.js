bdd82acf39dda1aea7c3d85667d6a71933ed29a5
Moha-Sameh <ms.slama01@hotmail.com>
2021-04-05 19:20:21 +0200

    push

--- index.js
+++ index.js
@@ -11,13 +11,9 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World<h1>");
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server started on port ${process.env.PORT}`)
-);
-
 const run = async () => {
   try {
-    await db.sequelize.sync({ force: true });
+    await db.sequelize.sync({ force: false });
     console.log("Connection to the database successful!");
   } catch (error) {
     console.error("Error connecting to the database: ", error);
